36365b0617bbf3a7825c1b2384ce87144a1d9f88
Alex Mihaila <am@objectline.com>
2018-07-25 08:09:54 -0400

    Get rid of mplab files

--- bus.X/build/default/debug/src/main.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/debug/src/main.d  \
- build/default/debug/src/main.p1:  \
- src/main.c  \
-src/../inc/config.h 
--- bus.X/build/default/production/src/main.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/src/main.d  \
- build/default/production/src/main.p1:  \
- src/main.c  \
-src/../inc/config.h 
