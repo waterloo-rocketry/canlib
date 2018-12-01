ec909dff47bfef9c6f7a935f9e6a750d776d1dfc
Aaron Morrison <akmorris@edu.uwaterloo.ca>
2018-12-01 17:46:57 -0500

    First stab at DSPIC33EPxxxGP CAN driver
    Two things I don't like about it:
    1. Can only transmit one message at a time
    2. Reception of CAN messages is handled in an interrupt context,
       should be doable in a polling fashion
    
    Both of these are solveable, but I haven't put the time into them yet.

---
+++ dspic33epxxxgp50x/dspic33epxxxgp50x_can.h
@@ -0,0 +1,22 @@
+#ifndef PIC18F26K83_CAN_H_
+#define PIC18F26K83_CAN_H_
+
+#include "../can.h"
+
+/*
+ * Initialize the CAN driver on a PI18fC26fk83. Note that this function
+ * DOES NOT setup the inputs and outputs from the CAN module to the
+ * output pins, application code must do that. In order to do that,
+ * CANRXPPS must be set to the proper pin value for the CANRX pin, and
+ * ___PPS must be set to 0x33 to mark it as outputting from the CAN
+ * module. In addition, TRIS and ANSEL registers for whatever pin
+ * is being used must be set to the right values.
+ */
+void init_can(const can_timing_t *timing,
+              void (*receive_callback)(const can_msg_t *message));
+
+// send a CAN message. Priority is 3 or less, higher means higher
+// priority
+void can_send(const can_msg_t* message, uint8_t priority);
+
+#endif // compile guard
