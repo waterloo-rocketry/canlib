3e5fd7906ffc302d21333c6cb1d444ed07c88564
Alex Mihaila <am@objectline.com>
2019-02-16 14:19:04 -0800

    Add vertical indentation for message types

--- message_types.h
+++ message_types.h
@@ -14,15 +14,19 @@
 #define MSG_GENERAL_CMD           0x060
 #define MSG_VENT_VALVE_CMD        0x0C0
 #define MSG_INJ_VALVE_CMD         0x120
+
 #define MSG_DEBUG_MSG             0x180
 #define MSG_DEBUG_PRINTF          0x1E0
+
 #define MSG_VENT_VALVE_STATUS     0x460
 #define MSG_INJ_VALVE_STATUS      0x4C0
 #define MSG_GENERAL_BOARD_STATUS  0x520
+
 #define MSG_SENSOR_ACC            0x580
 #define MSG_SENSOR_GYRO           0x5E0
 #define MSG_SENSOR_MAG            0x640
 #define MSG_SENSOR_ANALOG         0x6A0
+
 #define MSG_LEDS_ON               0x7E0
 #define MSG_LEDS_OFF              0x7C0
 
