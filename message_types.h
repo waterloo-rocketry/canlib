428d05d934b38927fc988e7204893720465d41f6
Alex Mihaila <am@objectline.com>
2019-02-18 16:25:06 -0800

    Add message formats

--- message_types.h
+++ message_types.h
@@ -4,30 +4,15 @@
 /* The defines in this file track the information in this spreadsheet:
  * https://docs.google.com/spreadsheets/d/1sXmhBklBM_79vqq_NNaoq-xuxz7BbkHtFnsmBFPSksc/
  *
+ * If the spreadsheet and file differ, this file is the source of truth.
+ *
  * Message SIDs are 11 bit unique identifiers.
  * Bottom 5 bits: IDs that are unique to the board sending the message
- * Top 6 bits: message types. The message types defined here therefore 
+ * Top 6 bits: message types. The message types defined here therefore
  * have the bottom 5 bits set to 0.
  *
  * MESSAGE_SID = MESSAGE_TYPE | BOARD_UNIQUE_ID
  *
- * Message type format (from spreadsheet):
- * (Version 0.0.3)
- *
- * GENERAL CMD:     TSTAMP_MS_H	TSTAMP_MS_M	 TSTAMP_MS_L    COMMAND_TYPE	        None	        None	        None	        None
- * VENT_VALVE_CMD:  TSTAMP_MS_H	TSTAMP_MS_M	 TSTAMP_MS_L	VENT_VALVE_STATE	    None	        None	        None	        None
- * INJ_VALVE_CMD:   TSTAMP_MS_H	TSTAMP_MS_M	 TSTAMP_MS_L	INJ_VALVE_STATE	        None	        None	        None	        None
- * DEBUG_MSG:       TSTAMP_MS_H	TSTAMP_MS_M	 TSTAMP_MS_L	DEBUG_LEVEL | LINUM_H	LINUM_L	        MESSAGE_DEFINED	MESSAGE_DEFINED	MESSAGE_DEFINED
- * DEBUG_PRINTF:    ASCII	    ASCII	     ASCII	        ASCII	                ASCII	        ASCII	        ASCII	        ASCII
- * VENT_VALVE_STAT: TSTAMP_MS_H	TSTAMP_MS_M	 TSTAMP_MS_L	VENT_VALVE_STATE	    None	        None	        None	        None
- * INJ_VALVE_STAT:  TSTAMP_MS_H	TSTAMP_MS_M	 TSTAMP_MS_L	INJ_VALVE_STATE	        None	        None	        None	        None
- * BOARD_STAT:      TSTAMP_MS_H	TSTAMP_MS_M	 TSTAMP_MS_L	ERROR_CODE	            BOARD_DEFINED	BOARD_DEFINED	BOARD_DEFINED	BOARD_DEFINED
- * SENSOR_ACC:      TSTAMP_MS_M	TSTAMP_MS_L	 VALUE_X_H	    VALUE_X_L	            VALUE_Y_H	    VALUE_Y_L	    VALUE_Z_H	    VALUE_Z_L
- * SENSOR_GYRO:     TSTAMP_MS_M	TSTAMP_MS_L	 VALUE_X_H	    VALUE_X_L	            VALUE_Y_H	    VALUE_Y_L	    VALUE_Z_H	    VALUE_Z_L
- * SENSOR_MAG:      TSTAMP_MS_M	TSTAMP_MS_L	 VALUE_X_H	    VALUE_X_L	            VALUE_Y_H	    VALUE_Y_L	    VALUE_Z_H	    VALUE_Z_L
- * SENSOR_ANALOG:   TSTAMP_MS_M	TSTAMP_MS_L	 SENSOR_ID      VALUE_H	                VALUE_L	        None	        None	        None
- * LEDS_ON:         None	    None	     None	        None	                None	        None	        None	        None
- * LEDS_OFF:        None	    None	     None	        None	                None	        None	        None	        None
  */
 
 #define MSG_GENERAL_CMD           0x060
@@ -61,5 +46,84 @@
 #define BOARD_ID_USB_SPARE        0x0A
 #define BOARD_ID_VENT             0x0B
 #define BOARD_ID_VENT_SPARE       0x0C
+
+/*
+ * General message type format (from spreadsheet):
+ * (Version 0.0.3)
+ *                  byte 0      byte 1       byte 2         byte 3                  byte 4          byte 5          byte 6          byte 7
+ * GENERAL CMD:     TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    COMMAND_TYPE            None            None            None            None
+ * VENT_VALVE_CMD:  TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    VENT_VALVE_STATE        None            None            None            None
+ * INJ_VALVE_CMD:   TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    INJ_VALVE_STATE         None            None            None            None
+ * DEBUG_MSG:       TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    DEBUG_LEVEL | LINUM_H   LINUM_L         MESSAGE_DEFINED MESSAGE_DEFINED MESSAGE_DEFINED
+ * DEBUG_PRINTF:    ASCII       ASCII        ASCII          ASCII                   ASCII           ASCII           ASCII           ASCII
+ * VENT_VALVE_STAT: TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    VENT_VALVE_STATE        CMD_VALVE_STATE None            None            None
+ * INJ_VALVE_STAT:  TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    INJ_VALVE_STATE         CMD_VALVE_STATE None            None            None
+ * BOARD_STAT:      TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    ERROR_CODE              BOARD_DEFINED   BOARD_DEFINED   BOARD_DEFINED   BOARD_DEFINED
+ * SENSOR_ACC:      TSTAMP_MS_M TSTAMP_MS_L  VALUE_X_H      VALUE_X_L               VALUE_Y_H       VALUE_Y_L       VALUE_Z_H       VALUE_Z_L
+ * SENSOR_GYRO:     TSTAMP_MS_M TSTAMP_MS_L  VALUE_X_H      VALUE_X_L               VALUE_Y_H       VALUE_Y_L       VALUE_Z_H       VALUE_Z_L
+ * SENSOR_MAG:      TSTAMP_MS_M TSTAMP_MS_L  VALUE_X_H      VALUE_X_L               VALUE_Y_H       VALUE_Y_L       VALUE_Z_H       VALUE_Z_L
+ * SENSOR_ANALOG:   TSTAMP_MS_M TSTAMP_MS_L  SENSOR_ID      VALUE_H                 VALUE_L         None            None            None
+ * LEDS_ON:         None        None         None           None                    None            None            None            None
+ * LEDS_OFF:        None        None         None           None                    None            None            None            None
+ *
+ * This file defines the format of the various CAN message types (defined in
+ * message_types.h). There is no unified message format; the format of each message
+ * depends on the message type. In the case of board status messages, the format
+ * of the rest of the message depends on the message's error code.
+ *
+ * This file is the source of truth for message formats. Refer to this when parsing
+ * CAN messages. Increment the version number when the format for a new message
+ * type is added.
+ */
+
+// GENERAL_CMD: COMMAND_TYPE
+enum GEN_CMD {
+    BUS_DOWN_WARNING = 0,
+};
+
+// VALVE_CMD/STATUS STATES
+enum VALVE_STATE {
+    VALVE_OPEN = 0,
+    VALVE_CLOSED,
+    VALVE_UNK,
+};
+
+// BOARD GENERAL STATUS ERROR CODES
+//  ERROR CODE (byte 3)         (byte4)             (byte 5)            (byte 6)            (byte 7)
+enum BOARD_STATUS {
+    E_NOMINAL = 0,              // x                x                   x                   x
+
+    E_BUS_OVER_CURRENT,         // mA_high          mA_low              x                   x
+    E_BUS_UNDER_VOLTAGE,        // mV_high          mV_low              x                   x
+    E_BUS_OVER_VOLTAGE,         // mV_high          mV_low              x                   x
+
+    E_BATT_UNDER_VOLTAGE,       // mV_high          mV_low              x                   x
+    E_BATT_OVER_VOLTAGE,        // mV_high          mV_low              x                   x
+
+    E_BOARD_FEARED_DEAD,        // board_id         x                   x                   x
+    E_NO_CAN_TRAFFIC,           // time_s_high      time_s_low          x                   x
+    E_MISSING_CRITICAL_BOARD,   // board_id         x                   x                   x
+    E_RADIO_SIGNAL_LOST,        // time_s_high      time_s_low          x                   x
+
+    E_VALVE_STATE,              // expected_state   valve_state         x                   x
+    E_CANNOT_INIT_DACS,         // x                x                   x                   x
+    E_VENT_POT_RANGE,           // lim_upper (mV)   lim_lower (mV)      pot (mV)            x
+
+    E_LOGGING,                  // x                x                   x                   x
+    E_GPS,                      // x                x                   x                   x
+    E_SENSOR,                   // sensor_id        x                   x                   x
+
+    E_ILLEGAL_CAN_MSG,          // x                x                   x                   x
+    E_SEGFAULT,                 // x                x                   x                   x
+    E_UNHANDLED_INTERRUPT,      // x                x                   x                   x
+};
+
+enum SENSOR_ID {
+    SENSOR_IMU1 = 0,
+    SENSOR_IMU2,
+    SENSOR_BARO,
+    SENSOR_PRESSURE,
+};
+
 
 #endif // compile guard
