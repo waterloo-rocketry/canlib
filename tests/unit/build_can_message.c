e677b4429e74b3c29392ed80c383c7388192480d
Alex Mihaila <am@objectline.com>
2019-03-10 15:27:03 -0700

    Review changes
    - change timestamp function name
    - change can msg build functions to return bools instead of using
    assertions
    - change tests to keep running after one test has failed
    - add sensor data & timestamp getters
    - add more tests for different message types
    - make comments prettier

--- tests/unit/build_can_message.c
+++ tests/unit/build_can_message.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 //if this test is running on hardware, don't actually print anything
-#ifndef NO_PRINTF
+#ifdef TARGET_LOCAL
 #define REPORT_FAIL(x) printf("%s: Fail: %s\n", __FUNCTION__, x)
 #else
 #define REPORT_FAIL(x)
@@ -17,28 +17,158 @@ static bool test_general_command(void)
     uint8_t input_data = 0x74;
     can_msg_t output;
 
+    bool ret = true;
+
+    // Test illegal args
+    if (build_general_cmd_msg(timestamp, input_data, NULL)) {
+        REPORT_FAIL("Message built with null output");
+        ret = false;
+    }
+
     // make sure we don't get an error creating the message
-    build_general_cmd_msg(timestamp, input_data, &output);
+    if (!build_general_cmd_msg(timestamp, input_data, &output)) {
+        REPORT_FAIL("Error building message");
+        ret = false;
+    }
 
     // check that the data length is 4
     if (output.data_len != 4) {
         REPORT_FAIL("Wrong data_len generated");
-        return false;
+        ret = false;
     }
     // check that the timestamp was copied across properly
-    if (output.data[0] != 0xfe ||
-        output.data[1] != 0xba ||
-        output.data[2] != 0xbe) {
+    // only bottom 3 bytes of original should be copied
+    if (get_timestamp(&output) != (timestamp & 0xffffff)) {
         REPORT_FAIL("Timestamp copied wrong");
-        return false;
+        ret = false;
     }
     // check that the SID is proper. Note that the Makefile defines
     // the unique ID to be 0x3
     if (output.sid != 0x063) {
         REPORT_FAIL("SID compare failed");
-        return false;
+        ret = false;
     }
-    return true;
+
+    return ret;
+}
+
+static bool test_valve_cmd(void)
+{
+    uint32_t timestamp = 0x12345678;
+    can_msg_t output;
+
+    bool ret = true;
+
+    // test illegal args
+    if (build_valve_cmd_msg(timestamp, VALVE_OPEN, MSG_DEBUG_MSG, &output)) {
+        REPORT_FAIL("Valve cmd built with invalid message type");
+        ret = false;
+    }
+    if (build_valve_cmd_msg(timestamp, VALVE_OPEN, MSG_INJ_VALVE_CMD, NULL)) {
+        REPORT_FAIL("Message built with null output");
+        ret = false;
+    }
+
+    // test nominal behaviour
+    if (!build_valve_cmd_msg(timestamp, VALVE_OPEN, MSG_INJ_VALVE_CMD, &output)) {
+        REPORT_FAIL("Error building message");
+        ret = false;
+    }
+    if (output.data_len != 4) {
+        REPORT_FAIL("Data length is wrong");
+        ret = false;
+    }
+    if (get_timestamp(&output) != (timestamp & 0xffffff)) {
+        REPORT_FAIL("Timestamp copied wrong");
+        ret = false;
+    }
+    if (get_curr_valve_state(&output) != -1) {
+        REPORT_FAIL("Got current valve state from a command message");
+        ret = false;
+    }
+    if (get_req_valve_state(&output) != VALVE_OPEN) {
+        REPORT_FAIL("Valve data copied incorrectly");
+        ret = false;
+    }
+
+    return ret;
+}
+
+static bool test_valve_stat(void)
+{
+    uint32_t timestamp = 0x12345678;
+    can_msg_t output;
+
+    bool ret = true;
+
+    // test illegal args
+    if (build_valve_stat_msg(timestamp, VALVE_OPEN, VALVE_UNK, MSG_DEBUG_MSG, &output)) {
+        REPORT_FAIL("Valve cmd built with invalid message type");
+        ret = false;
+    }
+    if (build_valve_stat_msg(timestamp, VALVE_OPEN, VALVE_UNK, MSG_INJ_VALVE_STATUS, NULL)) {
+        REPORT_FAIL("Message built with null output");
+        ret = false;
+    }
+
+    // test nominal behaviour
+    if (!build_valve_stat_msg(timestamp, VALVE_OPEN, VALVE_CLOSED, MSG_INJ_VALVE_STATUS, &output)) {
+        REPORT_FAIL("Error building message");
+        ret = false;
+    }
+    if (output.data_len != 5) {
+        REPORT_FAIL("Data length is wrong");
+        ret = false;
+    }
+    if (get_timestamp(&output) != (timestamp & 0xffffff)) {
+        REPORT_FAIL("Timestamp copied wrong");
+        ret = false;
+    }
+    if (get_curr_valve_state(&output) != VALVE_OPEN) {
+        REPORT_FAIL("Current valve state copied wrong");
+        ret = false;
+    }
+    if (get_req_valve_state(&output) != VALVE_CLOSED) {
+        REPORT_FAIL("Requested valve state copied wrong");
+        ret = false;
+    }
+
+    return ret;
+}
+
+static bool test_board_stat(void)
+{
+    uint32_t timestamp = 0x12345678;
+    uint8_t error_data[2] = { 0x32, 0x10 };
+    uint8_t error_data_len = 2;
+    can_msg_t output;
+
+    bool ret = true;
+
+    // test illegal args
+    if (build_board_stat_msg(timestamp, E_NOMINAL, NULL, error_data_len, &output)) {
+        REPORT_FAIL("Built message with null input data");
+        ret = false;
+    }
+    if (build_board_stat_msg(timestamp, E_NOMINAL, error_data, error_data_len, NULL)) {
+        REPORT_FAIL("Built message with null output");
+        ret = false;
+    }
+
+    // test nominal behaviour
+    if (!build_board_stat_msg(timestamp, E_BATT_UNDER_VOLTAGE, error_data, error_data_len, &output)) {
+        REPORT_FAIL("Error building message");
+        ret = false;
+    }
+    if (get_timestamp(&output) != (timestamp & 0xffffff)) {
+        REPORT_FAIL("Timestamp copied wrong");
+        ret = false;
+    }
+    if (output.data_len != 6) {
+        REPORT_FAIL("Data length is wrong");
+    }
+
+    return ret;
 }
 
 static bool test_debug_printf(void)
@@ -48,12 +178,33 @@ static bool test_debug_printf(void)
         'u', 'n', 'i', 't',
         't', 'e', 's', 't'
     };
+
     can_msg_t output;
-    build_debug_printf(input_data, &output);
+    bool ret = true;
+
+    // Test illegal args
+    if (build_debug_printf(NULL, &output)) {
+        REPORT_FAIL("Message built with null data");
+        ret = false;
+    }
+
+    if (build_debug_printf(input_data, NULL)) {
+        REPORT_FAIL("Message built with null output");
+        ret = false;
+    }
+
+    if (!build_debug_printf(input_data, &output)) {
+        REPORT_FAIL("Error building message");
+        ret = false;
+    }
     // check the SID
     if (output.sid != 0x1E3) {
         REPORT_FAIL("SID compare failed");
-        return false;
+        ret = false;
+    }
+    if (get_timestamp(&output) != 0) {
+        REPORT_FAIL("Timestamp copied wrong");
+        ret = false;
     }
     // check the outputted data
     if (output.data[0] != 'u' ||
@@ -65,55 +216,151 @@ static bool test_debug_printf(void)
         output.data[6] != 's' ||
         output.data[7] != 't') {
         REPORT_FAIL("printf data not copied over properly");
-        return false;
+        ret = false;
     }
-    return true;
+    return ret;
 }
 
-static bool test_sensor_acc(void)
+static bool test_sensor_imu(void)
 {
     uint16_t input_data[3] = {
         0xa749, 0x6664, 0x1008
     };
     can_msg_t output;
     uint32_t timestamp = 0x12345678;
-    build_imu_data_msg(MSG_SENSOR_ACC, timestamp, input_data, &output);
-    
+    bool ret = true;
+
+    // test illegal args
+    if (build_imu_data_msg(MSG_GENERAL_CMD, timestamp, input_data, &output)) {
+        REPORT_FAIL("IMU message created with invalid message type");
+        ret = false;
+    }
+    if (build_imu_data_msg(MSG_SENSOR_ACC, timestamp, NULL, &output)) {
+        REPORT_FAIL("Message built with null input data");
+        ret = false;
+    }
+    if (build_imu_data_msg(MSG_SENSOR_ACC, timestamp, input_data, NULL)) {
+        REPORT_FAIL("Message built with null output");
+        ret = false;
+    }
+
+    // test nominal behaviour
+    if (!build_imu_data_msg(MSG_SENSOR_ACC, timestamp, input_data, &output)) {
+        REPORT_FAIL("Error building message");
+        ret = false;
+    }
     if (output.sid != 0x583) {
         REPORT_FAIL("SID compare failed");
-        printf("sid: %x\n", output.sid);
-        return false;
+        ret = false;
     }
-    if (output.data[0] != 0x56 ||
-        output.data[1] != 0x78) {
+    if (output.data_len != 8) {
+        REPORT_FAIL("Data length copied wrong");
+        ret = false;
+    }
+    if (get_timestamp(&output) != (timestamp & 0xffff)) {
         REPORT_FAIL("Timestamp copied wrong");
-        return false;
+        ret = false;
     }
-    if (output.data[2] != 0xa7 ||
-        output.data[3] != 0x49 ||
-        output.data[4] != 0x66 ||
-        output.data[5] != 0x64 ||
-        output.data[6] != 0x10 ||
-        output.data[7] != 0x08) {
-        REPORT_FAIL("Sensor Data copied wrong");
-        return false;
+
+    uint16_t output_x, output_y, output_z;
+    if (!get_imu_data(&output, &output_x, &output_y, &output_z)) {
+        REPORT_FAIL("Failed to retrieve sensor data");
+        ret = false;
     }
-    return true;
+    if (output_z != 0x1008) {
+        REPORT_FAIL("Z data is incorrect");
+        ret = false;
+    }
+    if (output_y != 0x6664) {
+        REPORT_FAIL("Y data is incorrect");
+        ret = false;
+    }
+    if (output_x != 0xa749) {
+        REPORT_FAIL("X data is incorrect");
+        ret = false;
+    }
+    return ret;
 }
 
+static bool test_sensor_analog(void)
+{
+    uint16_t data = 0x380c;
+    uint32_t timestamp = 0x12345678;
+    can_msg_t output;
+    bool ret = true;
+
+    // test illegal args
+    if (build_analog_data_msg(timestamp, SENSOR_BARO, data, NULL)) {
+        REPORT_FAIL("Message built with null output");
+        ret = false;
+    }
+
+    // test nominal behaviour
+    if (!build_analog_data_msg(timestamp, SENSOR_BARO, data, &output)) {
+        REPORT_FAIL("Error building message");
+        ret = false;
+    }
+    if (output.sid != 0x6a3) {
+        REPORT_FAIL("SID compare failed");
+        ret = false;
+    }
+    if (output.data_len != 5) {
+        REPORT_FAIL("Data length copied wrong");
+        ret = false;
+    }
+    if (get_timestamp(&output) != (timestamp & 0xffff)) {
+        REPORT_FAIL("Timestamp copied wrong");
+        ret = false;
+    }
+
+    uint16_t output_data;
+    uint8_t output_sensor_id;
+    if (!get_analog_data(&output, &output_sensor_id, &output_data)) {
+        REPORT_FAIL("Failed to retrieve sensor data");
+        ret = false;
+    }
+    if (output_data != data) {
+        REPORT_FAIL("Analog data is incorrect");
+        ret = false;
+    }
+    if (output_sensor_id != SENSOR_BARO) {
+        REPORT_FAIL("Sensor id is incorrect");
+        ret = false;
+    }
+    return ret;
+}
 
 bool test_build_can_message(void)
 {
+    bool ret = true;
     if (!test_general_command()) {
-        printf(__FILE__ ": Error, test_general_command returned false\n");
-        return false;
-    } else if (!test_debug_printf()) {
+        REPORT_FAIL("test_general_command returned false");
+        ret = false;
+    }
+    if (!test_valve_cmd()) {
+        REPORT_FAIL("test_valve_cmd returned false");
+        ret = false;
+    }
+    if (!test_valve_stat()) {
+        REPORT_FAIL("test_valve_stat returned false");
+        ret = false;
+    }
+    if (!test_board_stat()) {
+        REPORT_FAIL("test_board_stat returned false");
+        ret = false;
+    }
+    if (!test_debug_printf()) {
         REPORT_FAIL("test_debug_printf returned false");
-        return false;
-    } else if (!test_sensor_acc()) {
+        ret = false;
+    }
+    if (!test_sensor_imu()) {
         REPORT_FAIL("test_sensor_acc returned false");
-        return false;
+        ret = false;
+    }
+    if (!test_sensor_analog()) {
+        REPORT_FAIL("test_sensor_analog returned false");
+        ret = false;
     }
 
-    return true;
+    return ret;
 }
--- tests/unit/can_common_tests.c
+++ tests/unit/can_common_tests.c
@@ -17,37 +17,50 @@ static bool test_message_debug_level(void)
 {
     can_msg_t output;
     uint8_t input_data[8];
+    bool ret = true;
 
     // Check for ERROR
     input_data[0] = 0x10;
-    build_debug_msg(0, input_data, &output);
+    if (!build_debug_msg(0, input_data, &output)) {
+        REPORT_FAIL("fail on build_can_message when level is ERROR");
+        ret = false;
+    }
     if (ERROR != message_debug_level(&output)) {
         REPORT_FAIL("fail on message_debug_level when level is ERROR");
-        return false;
+        ret = false;
     }
 
     // Check for WARN
     input_data[0] = 0x20;
-    build_debug_msg(0, input_data, &output);
+    if (!build_debug_msg(0, input_data, &output)) {
+        REPORT_FAIL("fail on build_can_message when level is WARN");
+        ret = false;
+    }
     if (WARN != message_debug_level(&output)) {
         REPORT_FAIL("fail on message_debug_level when level is WARN");
-        return false;
+        ret = false;
     }
     // Check for INFO
     input_data[0] = 0x30;
-    build_debug_msg(0, input_data, &output);
+    if (!build_debug_msg(0, input_data, &output)) {
+        REPORT_FAIL("fail on build_can_message when level is INFO");
+        ret = false;
+    }
     if (INFO != message_debug_level(&output)) {
         REPORT_FAIL("fail on message_debug_level when level is INFO");
-        return false;
+        ret = false;
     }
     // Check for DEBUG
     input_data[0] = 0x40;
-    build_debug_msg(0, input_data, &output);
+    if (!build_debug_msg(0, input_data, &output)) {
+        REPORT_FAIL("fail on build_can_message when level is DEBUG");
+        ret = false;
+    }
     if (DEBUG != message_debug_level(&output)) {
         REPORT_FAIL("fail on message_debug_level when level is DEBUG");
-        return false;
+        ret = false;
     }
-    return true;
+    return ret;
 }
 
 static bool test_debug_printf(void)
@@ -55,12 +68,13 @@ static bool test_debug_printf(void)
     // test that debug printf works, and can write out "does this work lol?" (which should take 3 messages to write)
     can_msg_t output;
     const char *message = "does this work lol?";
+    bool ret = true;
 
     // this call should put "does thi" in output, and should set message to "s work lol?"
     message = build_printf_can_message(message, &output);
     if (output.data_len != 8) {
         REPORT_FAIL("First call to build_printf_can_message didn't set data_len properly");
-        return false;
+        ret = false;
     } else if (output.data[0] != 'd' ||
                output.data[1] != 'o' ||
                output.data[2] != 'e' ||
@@ -70,20 +84,20 @@ static bool test_debug_printf(void)
                output.data[6] != 'h' ||
                output.data[7] != 'i') {
         REPORT_FAIL("First call to build_printf_can_message didn't set data properly");
-        return false;
+        ret = false;
     } else if (strcmp(message, "s work lol?")) {
         REPORT_FAIL("First call to build_printf_can_message didn't set message properly");
-        return false;
+        ret = false;
     } else if (output.sid != 0x1E3) {
         REPORT_FAIL("First call to build_printf_can_message didn't set sid properly");
-        return false;
+        ret = false;
     }
 
     // this call should put "s work l" in output, and should set message to "ol?"
     message = build_printf_can_message(message, &output);
     if (output.data_len != 8) {
         REPORT_FAIL("Second call to build_printf_can_message didn't set data_len properly");
-        return false;
+        ret = false;
     } else if (output.data[0] != 's' ||
                output.data[1] != ' ' ||
                output.data[2] != 'w' ||
@@ -93,58 +107,47 @@ static bool test_debug_printf(void)
                output.data[6] != ' ' ||
                output.data[7] != 'l') {
         REPORT_FAIL("Second call to build_printf_can_message didn't set data properly");
-        return false;
+        ret = false;
     } else if (strcmp(message, "ol?")) {
         REPORT_FAIL("Second call to build_printf_can_message didn't set message properly");
-        return false;
+        ret = false;
     } else if (output.sid != 0x1E3) {
         REPORT_FAIL("Second call to build_printf_can_message didn't set sid properly");
-        return false;
+        ret = false;
     }
 
     // this call should put "ol?" in output, and should set message to '\0'
     message = build_printf_can_message(message, &output);
     if (output.data_len != 3) {
         REPORT_FAIL("Third call to build_printf_can_message didn't set data_len properly");
-        return false;
+        ret = false;
     } else if (output.data[0] != 'o' ||
                output.data[1] != 'l' ||
                output.data[2] != '?') {
         REPORT_FAIL("Third call to build_printf_can_message didn't set data properly");
-        return false;
+        ret = false;
     } else if (*message != '\0') {
         REPORT_FAIL("Third call to build_printf_can_message didn't set message properly");
-        return false;
+        ret = false;
     } else if (output.sid != 0x1E3) {
         REPORT_FAIL("Third call to build_printf_can_message didn't set sid properly");
-        return false;
+        ret = false;
     }
 
     // this call should put nothing in output, and shouldn't change message
     message = build_printf_can_message(message, &output);
     if (output.data_len != 0) {
         REPORT_FAIL("Fourth call to build_printf_can_message didn't set data_len properly");
-        return false;
+        ret = false;
     } else if (*message != '\0') {
         REPORT_FAIL("Fourth call to build_printf_can_message didn't set message properly");
-        return false;
+        ret = false;
     } else if (output.sid != 0x1E3) {
         REPORT_FAIL("Fourth call to build_printf_can_message didn't set sid properly");
-        return false;
+        ret = false;
     }
 
-
-    return true;
-}
-
-bool test_can_common_functions(void)
-{
-    if (!test_debug_printf()) {
-        REPORT_FAIL("Error, test_debug_printf returned false\n");
-        return false;
-    }
-
-    return true;
+    return ret;
 }
 
 bool test_debug_macro(void)
@@ -155,33 +158,55 @@ bool test_debug_macro(void)
     int linum = __LINE__ + 1;
     DEBUG(ERROR, 0, output);
 
+    bool ret = true;
+
     if (output.sid != 0x183) {
         // Check that the SID was set correctly. Note this assumes
         // that the unit tests assign BOARD_UNIQUE_ID to 0x03
-        return false;
+        ret = false;
     } else if (output.data_len != 8) {
         // Check that the proper amount of data was written out
-        return false;
+        ret = false;
     } else if (output.data[3] != (0x10 + ((linum >> 8) & 0xF))) {
         // Check that ERROR (0x01) was put in the top nibble, and that
         // bits [11:8] of linum (the line number of where that DEBUG
         // call is above) was put in the bottom nibble
-        return false;
+        ret = false;
     } else if (output.data[4] != (linum & 0xFF)) {
         // check that the bottom byte of linum was put in this byte of
         // the data
-        return false;
+        ret = false;
     } else if (message_debug_level(&output) != ERROR) {
         // Check the debug level. This is more of a test that the
         // message_debug_level function works, since we already
         // checked that the top nibble of data[3] was 0x01
-        return false;
+        ret = false;
     } else if (get_message_type(&output) != MSG_DEBUG_MSG) {
         // Check the message type. Again, we've already checked the
         // SID, so this is more of a check of the get_message_type
         // function
-        return false;
+        ret = false;
     }
 
-    return true;
+    return ret;
 }
+
+bool test_can_common_functions(void)
+{
+    bool ret = true;
+    if (!test_message_debug_level()) {
+        REPORT_FAIL("Error, test_message_debug_level returned false");
+        ret = false;
+    }
+    if (!test_debug_printf()) {
+        REPORT_FAIL("Error, test_debug_printf returned false");
+        ret = false;
+    }
+    if (!test_debug_macro()) {
+        REPORT_FAIL("Error, test_debug_macro returned false");
+        ret = false;
+    }
+
+    return ret;
+}
+
