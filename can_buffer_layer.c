227dc87d8e60d21635ab103f295e7758f891dc78
Anne Wang <wang.anne27@gmail.com>
2019-04-06 16:35:31 -0400

    Possibly working version one of a buffering layer

--- can_buffer_layer.c
+++ can_buffer_layer.c
@@ -1,15 +1,36 @@
 #include "can_buffer_layer.h"
+#include "safe_ring_buffer.h"
 
-void can_txb_init(bool (*is_transmit_buffer_empty)(void), void (*can_send)(const can_msg_t *msg, uint8_t priority));
+static srb_ctx_t buf;
+static cbl_ctx_t ctx;
 
-// returns false if the internal queue is out of room
-bool can_txb_queue(const can_msg_t *msg);
+void can_txb_init(void *pool, 
+                size_t pool_size,
+                bool (*is_transmit_buffer_empty)(void), 
+                void (*can_send)(const can_msg_t *msg, uint8_t priority)) {
+    ctx.is_transmit_buffer_empty = is_transmit_buffer_empty;
+    ctx.can_send = can_send;
+    srb_init(&buf, pool, pool_size, sizeof(can_msg_t));
+}
 
-void can_txb_something(void);
-
-bool is_transmit_buffer_empty() {
-    if (TXB0CONbits.TXREQ = 0) {
-        return true;
+bool can_txb_queue(const can_msg_t *msg, uint8_t priority) {
+    // if the internal queue is out of room, returns false
+    if (srb_is_full(&buf)) {
+        return false;
     }
-    return false;
+    // else, adds the message to the queue
+    msg_t can_msg = {
+        msg,
+        priority
+    };
+    srb_push(&buf, &can_msg);
+    return true;
+}
+
+void can_txb_check(void) {
+    if ((*(ctx.is_transmit_buffer_empty))()) {
+        msg_t can_msg;
+        srb_pop(&buf, &can_msg);
+        (*(ctx.can_send))(can_msg.msg, can_msg.priority);
+    }
 }
--- can_buffer_layer.h
+++ can_buffer_layer.h
@@ -5,9 +5,22 @@
 * header file for a buffering layer for messages being sent to can_send
 */
 
-void can_txb_init(bool (*is_transmit_buffer_empty)(void), void (*can_send)(const can_msg_t *msg, uint8_t priority));
+void can_txb_init(void *pool, 
+                size_t pool_size,
+                bool (*is_transmit_buffer_empty)(void), 
+                void (*can_send)(const can_msg_t *msg, uint8_t priority));
 
 // returns false if the internal queue is out of room
-bool can_txb_queue(const can_msg_t *msg);
+bool can_txb_queue(const can_msg_t *msg, uint8_t priority);
 
 void can_txb_something(void);
+
+typedef struct {
+    bool (*is_transmit_buffer_empty)(void);
+    void (*can_send)(const can_msg_t *msg, uint8_t priority);
+} cbl_ctx_t;
+
+typedef struct {
+    can_msg_t *msg;
+    uint8_t priority;
+} msg_t;
