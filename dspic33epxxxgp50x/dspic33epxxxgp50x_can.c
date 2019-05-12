a6e3c9f62d0247ddfc2c040f76cc63b544659a65
Alexander Kitaev <alex.kitaev58@gmail.com>
2019-05-12 11:34:33 -0400

    Fixed dspic can_send() function signature

--- dspic33epxxxgp50x/dspic33epxxxgp50x_can.c
+++ dspic33epxxxgp50x/dspic33epxxxgp50x_can.c
@@ -83,7 +83,7 @@ void init_can(const can_timing_t *timing,
 // Priority must be a 2 bit number defining how high the priority of
 // this message is vs the other ones queued to be sent. 0 is lowest
 // priority, 3 is highest
-void can_send(const can_msg_t* message, uint8_t priority) {
+void can_send(const can_msg_t* message) {
     // put the SID in buffer 0 of the DMA buffers
     can_msg_buf[0][0] = (message->sid << 2);
 
