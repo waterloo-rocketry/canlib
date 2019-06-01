2384d09fb1d49eff749d8d27d5b7aed57717bbba
Aaron Morrison <akmorris@edu.uwaterloo.ca>
2019-06-01 12:40:17 -0400

    Add timing params for logger (32MHz)

--- util/timing_util.c
+++ util/timing_util.c
@@ -8,6 +8,15 @@ bool can_generate_timing_params(uint32_t can_frequency, can_timing_t *timing)
 {
     // this function is designed to create a bit time of 24 microseconds
     switch (can_frequency) {
+        case 32000000:
+            timing->brp      = 31;
+            timing->sjw      =  3;
+            timing->btlmode  =  1;
+            timing->sam      =  0;
+            timing->seg1ph   =  4;
+            timing->prseg    =  0;
+            timing->seg2ph   =  4;
+            return true;
         case 12000000:
             timing->brp      = 11;
             timing->sjw      =  3;
