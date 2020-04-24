a9c812035f858f451c3935b3d580c0f0a96a7786
Zachariah Mears <zx100x100@gmail.com>
2020-04-24 02:21:05 -0400

    Implement build_arm_cmd_msg and build_arm_stat_msg functions.

--- can_common.c
+++ can_common.c
@@ -171,13 +171,13 @@ bool build_arm_cmd_msg(uint32_t timestamp,
 {
     if (!output) { return false; }
 
-    output->sid = MSG_ALT_ARM_STATUS | BOARD_UNIQUE_ID;
+    output->sid = MSG_ALT_ARM_CMD | BOARD_UNIQUE_ID;
     write_timestamp_3bytes(timestamp, output);
 
     // 4 msb are used for arm state, 4 lsb used for altimeter number
-    output->data[3] = arm_cmd << 4 | alt_num & 0x0F; 
+    output->data[3] = arm_cmd << 4 | alt_num & 0x0F;
     output->data_len = 4; // 3 bytes timestamp, 1 byte arm state + alt num
-    
+
     return true;
 }
 
@@ -194,7 +194,7 @@ bool build_arm_stat_msg(uint32_t timestamp,
     write_timestamp_3bytes(timestamp, output);
 
     // 4 msb are used for arm state, 4 lsb used for altimeter number
-    output->data[3] = arm_state << 4 | alt_num & 0x0F; 
+    output->data[3] = arm_state << 4 | alt_num & 0x0F;
     // drogue voltage
     output->data[4] = v_drogue >> 8;     // 8 msb
     output->data[5] = v_drogue & 0x00FF; // 8 lsb
