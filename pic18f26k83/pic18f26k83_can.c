44e0179b49434ef4470171e1b6a0578cbf9ab6c3
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2020-10-23 22:08:10 -0400

    Fix reading from RXBUF1 while RXBUF0 is full

--- pic18f26k83/pic18f26k83_can.c
+++ pic18f26k83/pic18f26k83_can.c
@@ -58,6 +58,8 @@ void can_init(const can_timing_t *timing,
     RXB0CONbits.RXM0 = 1;
     RXB0CONbits.RXM1 = 1;
 
+    // Allow overflowing of messages into RXBUF1 if RXBUF0 is full
+    RXB0CONbits.RB0DBEN = 1;
 
     // enable interrupts for all useful CAN interrupts
     //  interrupt triggered on invalid message received
@@ -140,12 +142,12 @@ void can_handle_interrupt() {
         RXB0CONbits.RXFUL = 0;
         return;
     } else if (PIR5bits.RXB1IF) {
-                
+
         can_msg_t rcvd_msg;
         rcvd_msg.sid = (((uint16_t)RXB1SIDH) << 3) | (RXB1SIDL >> 5);
         rcvd_msg.data_len = RXB1DLCbits.DLC;
         memcpy(rcvd_msg.data, (const void *) &RXB1D0, rcvd_msg.data_len);
-        
+
         // call application code callback
         if (NULL != can_rcv_cb) {
             can_rcv_cb(&rcvd_msg);
