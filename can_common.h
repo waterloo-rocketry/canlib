f07c720b691f0ee1430827535c07c12dca5d6be3
Zachariah Mears <zx100x100@gmail.com>
2021-01-12 16:58:22 -0500

    add recovery status message type and coresponding build and get functions.

--- can_common.h
+++ can_common.h
@@ -248,16 +248,22 @@ bool get_req_arm_state(const can_msg_t *msg, uint8_t *alt_num, enum ARM_STATE *a
 
 
 /*
-* Gets the current arm state and which altimiter it is for.
+* Gets the current arm state and which altimeter it is for.
 * Returns false if the provided message is not an arm status.
 */
 bool get_curr_arm_state(const can_msg_t *msg, uint8_t *alt_num, enum ARM_STATE *arm_state);
 
 /*
 * Gets the requested arm state and altimeter number.
-* Returns false if the provided mesage is not an arm state request."
+* Returns false if the provided message is not an arm state request."
 */
 bool get_req_arm_state(const can_msg_t *msg, uint8_t *alt_num, enum ARM_STATE *arm_state);
+
+/*
+ *Gets the remote arming recovery status
+ * Returns false if the provided message is not a recovery status message
+ */
+bool get_recovery_state(const can_msg_t *msg, enum RECOVERY_STATE *state);
 
 /*
  * Strips the board unique ID from msg, and returns the SID. Contains
@@ -316,6 +322,15 @@ bool get_altitude_data(const can_msg_t *msg,
 bool get_pyro_voltage_data(const can_msg_t *msg,
                            uint16_t *v_drogue,
                            uint16_t *v_main);
+
+/*
+ * Gets the voltages read from the outputs of the mag switches
+ * this indicates if the mag switches are on
+ * returns false if the message is not a RECOVERY_STATUS message
+ */
+bool get_mag_voltage_data(const can_msg_t *msg,
+                          uint16_t *v_mag1,
+                          uint16_t *vmag2);
 
 /*
  * Gets GPS UTC time information. Format is UTC hours, minutes,
