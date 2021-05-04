47c0defcf74658a6018d01fc86330603edd9e0e3
Christopher Chung <tps90067@gmail.com>
2021-05-03 20:12:29 -0400

    fix revision number

--- can.h
+++ can.h
@@ -9,7 +9,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define CANLIB_VERSION            "v0.4.1"
+#define CANLIB_VERSION            "v0.5.0"
 #define CANLIB_BIT_TIME_US        24
 
 // Timing parameters
