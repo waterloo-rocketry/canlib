9e308ae8bce2065df9b9b57a3d746813575ea4a5
Joe <joedolina@gmail.com>
2023-06-18 01:01:57 -0400

    Payload 2023 (#50)
    * fix paths to can.h in subdirectory header files
    
    * nobody saw that
    
    * Fix macro name conflict w/ XC8
    
    * Add timing params for 6MHz clock
    
    * add payload thermistors
    
    * Added state est message type
    
    * Updated state est message type format
    
    * add payload thermistors
    
    * rebase master
    
    * fix paths to can.h in subdirectory header files
    
    * nobody saw that
    
    * Fix macro name conflict w/ XC8
    
    * Add timing params for 6MHz clock
    
    * Merge branch 'payload-dev' of https://github.com/waterloo-rocketry/canlib into payload-dev
    
    * add payload thermistors
    
    * Merge branch 'payload-therm' of https://github.com/waterloo-rocketry/canlib into payload-therm
    
    Consolidate edits for payload
    
    * Merge remote-tracking branch 'origin/kalman-message' into payload-therm
    
    i am so done with git
    
    * kalman message defn
    
    * revert data to 32 bits
    
    * Cleaned up message formats
    
    * update grandpapa act commands
    
    * shift patload act IDs
    
    * state est to 0x300
    
    * payload priotity reshuffle
    
    * Replace radi with state est
    
    * get rid of deprecated radifuncs
    
    * uncomment mcp2515 stuff
    
    * Remove radiation message functions from the .h file
    
    Co-Authored-By: bay-computer <uwrocketry@gmail.com>
    Co-Authored-By: Robert Cai <xiaoyucai2015@gmail.com>
    Co-Authored-By: Ozayr Raazi <oraazi2004@gmail.com>
    Co-Authored-By: KavinSatheeskumar <kavinsatheesk@gmail.com>

--- message_types.h
+++ message_types.h
@@ -24,6 +24,7 @@
 #define MSG_DEBUG_PRINTF          0x1E0
 #define MSG_DEBUG_RADIO_CMD       0x200
 
+
 #define MSG_ALT_ARM_STATUS        0x440
 #define MSG_ACTUATOR_STATUS       0x460
 #define MSG_GENERAL_BOARD_STATUS  0x520
@@ -42,8 +43,9 @@
 #define MSG_GPS_ALTITUDE          0x720
 #define MSG_GPS_INFO              0x740
 
+
 #define MSG_FILL_LVL              0x780
-#define MSG_RADI_VALUE            0x7A0
+#define MSG_STATE_EST             0x7A0
 
 #define MSG_LEDS_ON               0x7E0
 #define MSG_LEDS_OFF              0x7C0
@@ -73,39 +75,42 @@
 /*
  * General message type format (from spreadsheet):
  * (Version 0.7.0)
- *                  byte 0      byte 1      byte 2      byte 3                byte 4         byte 5             byte 6          byte 7
- * GENERAL_CMD:     TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L COMMAND_TYPE          None           None               None            None
- * ACTUATOR_CMD:    TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ACTUATOR_ID           ACTUATOR_STATE None               None            None
- * ALT_ARM_CMD:     TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALT_ARM_STATE & #     None           None               None            None
- * RESET_CMD:       TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L BOARD_ID              None           None               None            None
+ *                  byte 0      byte 1      byte 2      byte 3                byte 4           byte 5             byte 6           byte 7
+ * GENERAL_CMD:     TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L COMMAND_TYPE          None             None               None             None
+ * ACTUATOR_CMD:    TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ACTUATOR_ID           ACTUATOR_STATE   None               None             None
+ * ALT_ARM_CMD:     TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALT_ARM_STATE & #     None             None               None             None
+ * RESET_CMD:       TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L BOARD_ID              None             None               None             None
  *
- * DEBUG_MSG:       TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L DEBUG_LEVEL | LINUM_H LINUM_L        MESSAGE_DEFINED    MESSAGE_DEFINED MESSAGE_DEFINED
- * DEBUG_PRINTF:    ASCII       ASCII       ASCII       ASCII                 ASCII          ASCII              ASCII           ASCII
- * DEBUG_RADIO_CMD: ASCII       ASCII       ASCII       ASCII                 ASCII          ASCII              ASCII           ASCII
  *
- * ACTUATOR_STAT:   TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ACTUATOR_ID           ACTUATOR_STATE REQ_ACTUATOR_STATE None            None
- * ALT_ARM_STAT:    TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALT_ARM_STATE & #     V_DROGUE_H     V_DROGUE_L         V_MAIN_H        V_MAIN_L
- * BOARD_STAT:      TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ERROR_CODE            BOARD_DEFINED  BOARD_DEFINED      BOARD_DEFINED   BOARD_DEFINED
+ * DEBUG_MSG:       TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L DEBUG_LEVEL | LINUM_H LINUM_L          MESSAGE_DEFINED    MESSAGE_DEFINED  MESSAGE_DEFINED
+ * DEBUG_PRINTF:    ASCII       ASCII       ASCII       ASCII                 ASCII            ASCII              ASCII            ASCII
+ * DEBUG_RADIO_CMD: ASCII       ASCII       ASCII       ASCII                 ASCII            ASCII              ASCII            ASCII
  *
- * SENSOR_TEMP:     TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L SENSOR_NUM            TEMP_H         TEMP_M             TEMP_L          None
- * SENSOR_ALTITUDE: TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALTITUDE_H            ALTITUDE_MH    ALTITUDE_ML        ALTITUDE_L      None
- * SENSOR_ACC:      TSTAMP_MS_M TSTAMP_MS_L VALUE_X_H   VALUE_X_L             VALUE_Y_H      VALUE_Y_L          VALUE_Z_H       VALUE_Z_L
- * SENSOR_GYRO:     TSTAMP_MS_M TSTAMP_MS_L VALUE_X_H   VALUE_X_L             VALUE_Y_H      VALUE_Y_L          VALUE_Z_H       VALUE_Z_L
- * SENSOR_MAG:      TSTAMP_MS_M TSTAMP_MS_L VALUE_X_H   VALUE_X_L             VALUE_Y_H      VALUE_Y_L          VALUE_Z_H       VALUE_Z_L
- * SENSOR_ANALOG:   TSTAMP_MS_M TSTAMP_MS_L SENSOR_ID   VALUE_H               VALUE_L        None               None            None
  *
- * GPS_TIMESTAMP:   TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L UTC_HOURS             UTC_MINUTES    UTC_SECONDS        UTC_DSECONDS    None
- * GPS_LAT:         TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L DEGREES               MINUTES        DMINUTES_H         DIMNUTES_L      N/S DIRECTION
- * GPS_LON:         TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L DEGREES               MINUTES        DMINUTES_H         DIMNUTES_L      E/W DIRECTION
- * GPS_ALT:         TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALT_H                 ALT_L          ALT_DEC            UNITS           None
- * GPS_INFO:        TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L NUM_SAT               QUALITY        None               None            None
+ * ACTUATOR_STAT:   TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ACTUATOR_ID           ACTUATOR_STATE   REQ_ACTUATOR_STATE None             None
+ * ALT_ARM_STAT:    TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALT_ARM_STATE & #     V_DROGUE_H       V_DROGUE_L         V_MAIN_H         V_MAIN_L
+ * BOARD_STAT:      TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ERROR_CODE            BOARD_DEFINED    BOARD_DEFINED      BOARD_DEFINED    BOARD_DEFINED
  *
- * FILL_LVL:        TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L FILL_LEVEL            DIRECTION      None               None            None
+ * SENSOR_TEMP:     TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L SENSOR_NUM            TEMP_H           TEMP_M             TEMP_L           None
+ * SENSOR_ALTITUDE: TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALTITUDE_H            ALTITUDE_MH      ALTITUDE_ML        ALTITUDE_L       None
+ * SENSOR_ACC:      TSTAMP_MS_M TSTAMP_MS_L VALUE_X_H   VALUE_X_L             VALUE_Y_H        VALUE_Y_L          VALUE_Z_H        VALUE_Z_L
+ * SENSOR_GYRO:     TSTAMP_MS_M TSTAMP_MS_L VALUE_X_H   VALUE_X_L             VALUE_Y_H        VALUE_Y_L          VALUE_Z_H        VALUE_Z_L
+ * SENSOR_MAG:      TSTAMP_MS_M TSTAMP_MS_L VALUE_X_H   VALUE_X_L             VALUE_Y_H        VALUE_Y_L          VALUE_Z_H        VALUE_Z_L
+ * SENSOR_ANALOG:   TSTAMP_MS_M TSTAMP_MS_L SENSOR_ID   VALUE_H               VALUE_L          None               None             None
  *
- * RADI_VALUE:      TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L RADI_BOARD            RADI_HIGH      RADI_LOW           None            None
+ * GPS_TIMESTAMP:   TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L UTC_HOURS             UTC_MINUTES      UTC_SECONDS        UTC_DSECONDS     None
+ * GPS_LAT:         TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L DEGREES               MINUTES          DMINUTES_H         DIMNUTES_L       N/S DIRECTION
+ * GPS_LON:         TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L DEGREES               MINUTES          DMINUTES_H         DIMNUTES_L       E/W DIRECTION
+ * GPS_ALT:         TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L ALT_H                 ALT_L            ALT_DEC            UNITS            None
+ * GPS_INFO:        TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L NUM_SAT               QUALITY          None               None             None
+ * 
  *
- * LEDS_ON:         None        None        None        None                  None           None               None            None
- * LEDS_OFF:        None        None        None        None                  None           None               None            None
+ * FILL_LVL:        TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L FILL_LEVEL            DIRECTION        None               None             None
+ *
+ * STATE_EST:     TSTAMP_MS_H TSTAMP_MS_M TSTAMP_MS_L KALMAN_H       KALMAN_MH  KALMAN_ML    KALMAN_L  KALMAN_ID
+ *
+ * LEDS_ON:         None        None        None        None                  None             None               None             None
+ * LEDS_OFF:        None        None        None        None                  None             None               None             None
  *
  * This file defines the format of the various CAN message types (defined in
  * message_types.h). There is no unified message format; the format of each message
@@ -185,6 +190,8 @@ enum SENSOR_ID {
     SENSOR_VELOCITY,
     SENSOR_VENT_TEMP,
     SENSOR_RADIO_CURR,
+    SENSOR_PAYLOAD_TEMP_1,
+    SENSOR_PAYLOAD_TEMP_2
 };
 
 enum FILL_DIRECTION {
@@ -200,6 +207,7 @@ enum ACTUATOR_ID {
     ACTUATOR_CANBUS,
     ACTUATOR_CHARGE,
     ACTUATOR_RADIO,
+    ACTUATOR_PAYLOAD_5V,
 };
 
 #endif // compile guard
