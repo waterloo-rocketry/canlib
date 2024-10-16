869f95e49472cb81cab9ed701339c1ddfcddfe1f
taisirhassan <taisirisawesome@gmail.com>
2024-10-16 15:42:40 -0400

    Finished porting over low pass filter logic

--- include/low_pass_filter.h
+++ include/low_pass_filter.h
@@ -2,7 +2,12 @@
 #define ROCKETLIB_LOW_PASS_FILTER_H
 
 #include "common.h"
+#include <xc.h>
 
-w_status_t low_pass_filter_init(void);
+#define SAMPLE_FREQ(time_diff_ms) (1000.0 / (time_diff_ms))
+#define LOW_PASS_ALPHA(TR) ((SAMPLE_FREQ * TR / 5.0) / (1 + SAMPLE_FREQ * TR / 5.0))
+
+w_status_t low_pass_filter_init(double response_time);
+uint16_t update_low_pass(uint16_t new_value, double *low_pass_value);
 
 #endif /* ROCKETLIB_LOW_PASS_FILTER_H */
