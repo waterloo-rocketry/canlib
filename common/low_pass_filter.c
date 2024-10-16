a4731cad0c2be2c889b82ca6bf3164ab23181f73
taisirhassan <taisirisawesome@gmail.com>
2024-10-16 15:42:40 -0400

    Finished porting over low pass filter logic

--- common/low_pass_filter.c
+++ common/low_pass_filter.c
@@ -1,5 +1,20 @@
 #include "low_pass_filter.h"
 
-status_t low_pass_filter_init(void) {
-    return W_FAILURE;
+static volatile double alpha_low;
+
+status_t low_pass_filter_init(double response_time) {
+    if (response_time <= 0) {
+        return W_FAILURE;
+    }
+    alpha_low = LOW_PASS_ALPHA(response_time);
+    return W_SUCCESS;
+}
+
+uint16_t update_low_pass(uint16_t new_value, double *low_pass_value) {
+    if (low_pass_value == NULL) {
+        return 0;
+    }
+
+    *low_pass_value = alpha_low * (*low_pass_value) + (1.0 - alpha_low) * new_value;
+    return (uint16_t)(*low_pass_value);
 }
