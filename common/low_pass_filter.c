0a65caa3c953c14566088c26d42e4f460e79d2b9
taisirhassan <taisirisawesome@gmail.com>
2024-10-16 20:12:30 -0400

    Added comments and made alpha_low a pointer in order to allow multiple low pass filters to be initialized

--- common/low_pass_filter.c
+++ common/low_pass_filter.c
@@ -1,20 +1,21 @@
 #include "low_pass_filter.h"
 
-static volatile double alpha_low;
-
-status_t low_pass_filter_init(double response_time) {
-    if (response_time <= 0) {
-        return W_FAILURE;
+// Initialize the low-pass filter
+status_t low_pass_filter_init(double *alpha, double response_time) {
+    if (alpha == NULL || response_time <= 0) {
+        return W_FAILURE; // Return failure if the response time is invalid or alpha is NULL
     }
-    alpha_low = LOW_PASS_ALPHA(response_time);
-    return W_SUCCESS;
+    *alpha = LOW_PASS_ALPHA(response_time); // Calculate and store the alpha value
+    return W_SUCCESS; // Return success
 }
 
-uint16_t update_low_pass(uint16_t new_value, double *low_pass_value) {
-    if (low_pass_value == NULL) {
-        return 0;
+// Update the low-pass filter with a new value
+uint16_t update_low_pass(double *alpha, uint16_t new_value, double *low_pass_value) {
+    if (alpha == NULL || low_pass_value == NULL) {
+        return 0; // Return 0 if alpha or low_pass_value is NULL
     }
 
-    *low_pass_value = alpha_low * (*low_pass_value) + (1.0 - alpha_low) * new_value;
-    return (uint16_t)(*low_pass_value);
+    // Update the low-pass filter value
+    *low_pass_value = *alpha * (*low_pass_value) + (1.0 - *alpha) * new_value;
+    return (uint16_t)(*low_pass_value); // Return the updated low-pass filter value
 }
--- include/low_pass_filter.h
+++ include/low_pass_filter.h
@@ -4,10 +4,16 @@
 #include "common.h"
 #include <xc.h>
 
+// Macro to calculate the sample frequency based on the time difference in milliseconds
 #define SAMPLE_FREQ(time_diff_ms) (1000.0 / (time_diff_ms))
+
+// Macro to calculate the alpha value for the low-pass filter based on the response time
 #define LOW_PASS_ALPHA(TR) ((SAMPLE_FREQ * TR / 5.0) / (1 + SAMPLE_FREQ * TR / 5.0))
 
-status_t low_pass_filter_init(double response_time);
-uint16_t update_low_pass(uint16_t new_value, double *low_pass_value);
+// Initializes the low-pass filter by calculating the alpha value based on the response time
+status_t low_pass_filter_init(double *alpha, double response_time);
+
+// Updates the low-pass filter with a new value and returns the updated filter value
+uint16_t update_low_pass(double *alpha, uint16_t new_value, double *low_pass_value);
 
 #endif /* ROCKETLIB_LOW_PASS_FILTER_H */
