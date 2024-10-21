65fad361f700709abb8e96a6e50d946bc66393dd
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2024-10-21 10:51:11 -0400

    Add rocketlib assert function

--- include/common.h
+++ include/common.h
@@ -6,4 +6,19 @@ typedef enum {
     W_FAILURE
 } w_status_t;
 
+void w_assert_fail(const char *file, int line, const char *statement);
+
+#ifdef W_DEBUG
+
+#define w_assert(statement)                                                                        \
+    if (!(statement)) {                                                                            \
+        w_assert_fail(__FILE__, __LINE__, #statement);                                             \
+    }
+
+#else
+
+#define w_assert(statement)
+
+#endif
+
 #endif
