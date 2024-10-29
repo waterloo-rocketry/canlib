waterloo-rocketry/rocketlib

Added comments and made alpha_low a pointer in order to allow multiple low pass filters to be initialized

include/low_pass_filter.h
@@ -4,10 +4,16 @@
 #include "common.h"
 #include <xc.h>
 
+// Macro to calculate the sample frequency based on the time difference in milliseconds
 #define SAMPLE_FREQ(time_diff_ms) (1000.0 / (time_diff_ms))
+
+// Macro to calculate the alpha value for the low-pass filter based on the response time
 #define LOW_PASS_ALPHA(TR) ((SAMPLE_FREQ * TR / 5.0) / (1 + SAMPLE_FREQ * TR / 5.0))
 
-status_t low_pass_filter_init(double response_time);
-uint16_t update_low_pass(uint16_t new_value, double *low_pass_value);
+// Initializes the low-pass filter by calculating the alpha value based on the response time
+status_t low_pass_filter_init(double *alpha, double response_time);
+
+// Updates the low-pass filter with a new value and returns the updated filter value
+uint16_t update_low_pass(double *alpha, uint16_t new_value, double *low_pass_value);
 
 #endif /* ROCKETLIB_LOW_PASS_FILTER_H */

waterloo-rocketry/canlib

include/low_pass_filter.h
@@ -4,10 +4,16 @@
 #include "common.h"
 #include <xc.h>
 
+// Macro to calculate the sample frequency based on the time difference in milliseconds
 #define SAMPLE_FREQ(time_diff_ms) (1000.0 / (time_diff_ms))
+
+// Macro to calculate the alpha value for the low-pass filter based on the response time
 #define LOW_PASS_ALPHA(TR) ((SAMPLE_FREQ * TR / 5.0) / (1 + SAMPLE_FREQ * TR / 5.0))
 
-w_status_t low_pass_filter_init(double response_time);
-uint16_t update_low_pass(uint16_t new_value, double *low_pass_value);
+// Initializes the low-pass filter by calculating the alpha value based on the response time
+w_status_t low_pass_filter_init(double *alpha, double response_time);
+
+// Updates the low-pass filter with a new value and returns the updated filter value
+uint16_t update_low_pass(double *alpha, uint16_t new_value, double *low_pass_value);
 
 #endif /* ROCKETLIB_LOW_PASS_FILTER_H */