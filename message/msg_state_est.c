85d827b5196ed7c157ddcc6acfa2895418fc0187
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-02-15 20:31:23 -0500

    Update state estimation message functions

--- message/msg_state_est.c
+++ message/msg_state_est.c
@@ -8,73 +8,35 @@
 #include "msg_state_est.h"
 
 bool build_state_est_data_msg(
-    can_msg_prio_t prio, uint32_t timestamp, const float *data, enum STATE_ID data_id,
+    can_msg_prio_t prio, uint16_t timestamp, can_state_est_id_t state_id, const float *state_data,
     can_msg_t *output
 ) {
     output->sid = SID(prio, MSG_STATE_EST_DATA);
-    write_timestamp_3bytes(timestamp, output);
-    memcpy(output->data + 3, data, sizeof(*data));
-    output->data[7] = data_id;
+    write_timestamp_2bytes(timestamp, output);
+    output->data[2] = state_id;
+    memcpy(output->data + 3, state_data, sizeof(*state_data));
+
     output->data_len = 8;
 
     return true;
 }
 
-bool build_state_est_calibration_msg(
-    can_msg_prio_t prio, uint32_t timestamp, uint8_t ack_flag, uint16_t apogee, can_msg_t *output
-) {
-    if (!output) {
-        return false;
-    }
-
-    output->sid = SID(prio, MSG_STATE_EST_CALIB);
-    write_timestamp_3bytes(timestamp, output);
-
-    output->data[3] = ack_flag;
-    output->data[4] = (apogee >> 8) & 0xff;
-    output->data[5] = apogee & 0xff;
-
-    output->data_len = 6;
-
-    return true;
-}
-
-bool get_state_est_data(const can_msg_t *msg, float *data, enum STATE_ID *data_id) {
+bool get_state_est_data(const can_msg_t *msg, can_state_est_id_t *state_id, float *state_data) {
     if (!msg) {
         return false;
     }
-    if (!data) {
+    if (!state_id) {
         return false;
     }
-    if (!data_id) {
+    if (!state_data) {
         return false;
     }
     if (get_message_type(msg) != MSG_STATE_EST_DATA) {
         return false;
     }
 
-    memcpy(data, msg->data + 3, sizeof(*data));
-    *data_id = msg->data[7];
-
-    return true;
-}
-
-bool get_state_est_calibration_msg(const can_msg_t *msg, uint8_t *ack_flag, uint16_t *apogee) {
-    if (!msg) {
-        return false;
-    }
-    if (!ack_flag) {
-        return false;
-    }
-    if (!apogee) {
-        return false;
-    }
-    if (get_message_type(msg) != MSG_STATE_EST_CALIB) {
-        return false;
-    }
-
-    *ack_flag = msg->data[3];
-    *apogee = msg->data[4] << 8 | msg->data[5];
+    *state_id = msg->data[2];
+    memcpy(state_data, msg->data + 3, sizeof(*state_data));
 
     return true;
 }
