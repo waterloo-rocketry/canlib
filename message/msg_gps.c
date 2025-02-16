e107cba2d822755d974483186651432d42e65975
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-02-15 20:43:46 -0500

    Update GPS message build/get functions

--- message/msg_gps.c
+++ message/msg_gps.c
@@ -7,7 +7,7 @@
 #include "msg_gps.h"
 
 bool build_gps_time_msg(
-    can_msg_prio_t prio, uint32_t timestamp, uint8_t utc_hours, uint8_t utc_mins, uint8_t utc_secs,
+    can_msg_prio_t prio, uint16_t timestamp, uint8_t utc_hours, uint8_t utc_mins, uint8_t utc_secs,
     uint8_t utc_dsecs, can_msg_t *output
 ) {
     if (!output) {
@@ -15,20 +15,20 @@ bool build_gps_time_msg(
     }
 
     output->sid = SID(prio, MSG_GPS_TIMESTAMP);
-    write_timestamp_3bytes(timestamp, output);
+    write_timestamp_2bytes(timestamp, output);
 
-    output->data[3] = utc_hours;
-    output->data[4] = utc_mins;
-    output->data[5] = utc_secs;
-    output->data[6] = utc_dsecs;
+    output->data[2] = utc_hours;
+    output->data[3] = utc_mins;
+    output->data[4] = utc_secs;
+    output->data[5] = utc_dsecs;
 
-    output->data_len = 7;
+    output->data_len = 6;
 
     return true;
 }
 
 bool build_gps_lat_msg(
-    can_msg_prio_t prio, uint32_t timestamp, uint8_t degrees, uint8_t minutes, uint16_t dminutes,
+    can_msg_prio_t prio, uint16_t timestamp, uint8_t degrees, uint8_t minutes, uint16_t dminutes,
     uint8_t direction, can_msg_t *output
 ) {
     if (!output) {
@@ -36,21 +36,21 @@ bool build_gps_lat_msg(
     }
 
     output->sid = SID(prio, MSG_GPS_LATITUDE);
-    write_timestamp_3bytes(timestamp, output);
+    write_timestamp_2bytes(timestamp, output);
 
-    output->data[3] = degrees;
-    output->data[4] = minutes;
-    output->data[5] = dminutes >> 8;
-    output->data[6] = dminutes & 0xFF;
-    output->data[7] = direction;
+    output->data[2] = degrees;
+    output->data[3] = minutes;
+    output->data[4] = dminutes >> 8;
+    output->data[5] = dminutes & 0xFF;
+    output->data[6] = direction;
 
-    output->data_len = 8;
+    output->data_len = 7;
 
     return true;
 }
 
 bool build_gps_lon_msg(
-    can_msg_prio_t prio, uint32_t timestamp, uint8_t degrees, uint8_t minutes, uint16_t dminutes,
+    can_msg_prio_t prio, uint16_t timestamp, uint8_t degrees, uint8_t minutes, uint16_t dminutes,
     uint8_t direction, can_msg_t *output
 ) {
     if (!output) {
@@ -58,21 +58,21 @@ bool build_gps_lon_msg(
     }
 
     output->sid = SID(prio, MSG_GPS_LONGITUDE);
-    write_timestamp_3bytes(timestamp, output);
+    write_timestamp_2bytes(timestamp, output);
 
-    output->data[3] = degrees;
-    output->data[4] = minutes;
-    output->data[5] = dminutes >> 8;
-    output->data[6] = dminutes & 0xFF;
-    output->data[7] = direction;
+    output->data[2] = degrees;
+    output->data[3] = minutes;
+    output->data[4] = dminutes >> 8;
+    output->data[5] = dminutes & 0xFF;
+    output->data[6] = direction;
 
-    output->data_len = 8;
+    output->data_len = 7;
 
     return true;
 }
 
 bool build_gps_alt_msg(
-    can_msg_prio_t prio, uint32_t timestamp, uint16_t altitude, uint8_t daltitude, uint8_t units,
+    can_msg_prio_t prio, uint16_t timestamp, uint16_t altitude, uint8_t daltitude, uint8_t units,
     can_msg_t *output
 ) {
     if (!output) {
@@ -80,32 +80,32 @@ bool build_gps_alt_msg(
     }
 
     output->sid = SID(prio, MSG_GPS_ALTITUDE);
-    write_timestamp_3bytes(timestamp, output);
+    write_timestamp_2bytes(timestamp, output);
 
-    output->data[3] = (altitude >> 8) & 0xff;
-    output->data[4] = (altitude >> 0) & 0xff;
-    output->data[5] = daltitude;
-    output->data[6] = units;
+    output->data[2] = (altitude >> 8) & 0xff;
+    output->data[3] = (altitude >> 0) & 0xff;
+    output->data[4] = daltitude;
+    output->data[5] = units;
 
-    output->data_len = 7;
+    output->data_len = 6;
 
     return true;
 }
 
 bool build_gps_info_msg(
-    can_msg_prio_t prio, uint32_t timestamp, uint8_t num_sat, uint8_t quality, can_msg_t *output
+    can_msg_prio_t prio, uint16_t timestamp, uint8_t num_sat, uint8_t quality, can_msg_t *output
 ) {
     if (!output) {
         return false;
     }
 
     output->sid = SID(prio, MSG_GPS_INFO);
-    write_timestamp_3bytes(timestamp, output);
+    write_timestamp_2bytes(timestamp, output);
 
-    output->data[3] = num_sat;
-    output->data[4] = quality;
+    output->data[2] = num_sat;
+    output->data[3] = quality;
 
-    output->data_len = 5;
+    output->data_len = 4;
 
     return true;
 }
@@ -133,10 +133,10 @@ bool get_gps_time(
         return false;
     }
 
-    *utc_hours = msg->data[3];
-    *utc_mins = msg->data[4];
-    *utc_secs = msg->data[5];
-    *utc_dsecs = msg->data[6];
+    *utc_hours = msg->data[2];
+    *utc_mins = msg->data[3];
+    *utc_secs = msg->data[4];
+    *utc_dsecs = msg->data[5];
 
     return true;
 }
@@ -163,10 +163,10 @@ bool get_gps_lat(
         return false;
     }
 
-    *degrees = msg->data[3];
-    *minutes = msg->data[4];
-    *dminutes = msg->data[5] << 8 | msg->data[6];
-    *direction = msg->data[7];
+    *degrees = msg->data[2];
+    *minutes = msg->data[3];
+    *dminutes = ((uint16_t)msg->data[4] << 8) | msg->data[5];
+    *direction = msg->data[6];
 
     return true;
 }
@@ -193,10 +193,10 @@ bool get_gps_lon(
         return false;
     }
 
-    *degrees = msg->data[3];
-    *minutes = msg->data[4];
-    *dminutes = msg->data[5] << 8 | msg->data[6];
-    *direction = msg->data[7];
+    *degrees = msg->data[2];
+    *minutes = msg->data[3];
+    *dminutes = ((uint16_t)msg->data[4] << 8) | msg->data[5];
+    *direction = msg->data[6];
 
     return true;
 }
@@ -218,9 +218,9 @@ bool get_gps_alt(const can_msg_t *msg, uint16_t *altitude, uint8_t *daltitude, u
         return false;
     }
 
-    *altitude = (uint16_t)msg->data[3] << 8 | msg->data[4];
-    *daltitude = msg->data[5];
-    *units = msg->data[6];
+    *altitude = ((uint16_t)msg->data[2] << 8) | msg->data[3];
+    *daltitude = msg->data[4];
+    *units = msg->data[5];
 
     return true;
 }
@@ -239,8 +239,8 @@ bool get_gps_info(const can_msg_t *msg, uint8_t *num_sat, uint8_t *quality) {
         return false;
     }
 
-    *num_sat = msg->data[3];
-    *quality = msg->data[4];
+    *num_sat = msg->data[2];
+    *quality = msg->data[3];
 
     return true;
 }
