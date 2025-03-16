2673641530ac4dfff7295828ca28fdc858bda1a7
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-03-16 11:21:59 -0400

    Add error status bitfield definition

--- message_types.h
+++ message_types.h
@@ -103,18 +103,6 @@ typedef enum {
 } can_board_inst_id_thermocouple_t;
 
 typedef enum {
-    E_NOMINAL = 0x00,
-    E_5V_OVER_CURRENT = 0x01,
-    E_5V_OVER_VOLTAGE = 0x02,
-    E_5V_UNDER_VOLTAGE = 0x04,
-    E_12V_OVER_CURRENT = 0x08,
-    E_12V_OVER_VOLTAGE = 0x10,
-    E_12V_UNDER_VOLTAGE = 0x20,
-    E_IO_ERROR = 0x40,
-    E_FS_ERROR = 0x80,
-} can_general_board_status_t;
-
-typedef enum {
     ACTUATOR_OX_INJECTOR_VALVE = 0x00,
     ACTUATOR_FUEL_INJECTOR_VALVE = 0x01,
     ACTUATOR_CHARGE_ENABLE = 0x02,
@@ -208,5 +196,16 @@ typedef enum {
     STATE_ID_CANARD_ANGLE = 0x0C,
     STATE_ID_ENUM_MAX = 0x0D,
 } can_state_est_id_t;
+
+typedef enum {
+    E_5V_OVER_CURRENT_OFFSET = 0x00,
+    E_5V_OVER_VOLTAGE_OFFSET = 0x01,
+    E_5V_UNDER_VOLTAGE_OFFSET = 0x02,
+    E_12V_OVER_CURRENT_OFFSET = 0x03,
+    E_12V_OVER_VOLTAGE_OFFSET = 0x04,
+    E_12V_UNDER_VOLTAGE_OFFSET = 0x05,
+    E_IO_ERROR_OFFSET = 0x06,
+    E_FS_ERROR_OFFSET = 0x07,
+} can_general_board_status_offset_t;
 
 #endif
