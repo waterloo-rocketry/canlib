cc7d55b37dddf5a4f8a335440eb7b6c8649c3047
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-06-17 19:10:38 -0400

    Improve common.h comment

--- include/common.h
+++ include/common.h
@@ -1,9 +1,11 @@
 #ifndef ROCKETLIB_COMMON_H
 #define ROCKETLIB_COMMON_H
 
+// Rocketlib Version
 #define ROCKETLIB_VERSION_MAJOR 2025
 #define ROCKETLIB_VERSION_MINOR 1
 
+// Standard Error Code
 typedef enum {
     W_SUCCESS = 0,
     W_FAILURE,
@@ -14,6 +16,7 @@ typedef enum {
     W_OVERFLOW
 } w_status_t;
 
+// Assertion Macro
 #ifdef __cplusplus
 extern "C" {
 #endif
