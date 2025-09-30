f565ae8d505f9be84eb6317c9700b665a9c46cfa
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-09-29 22:19:06 -0400

    Make format and lint script take executable path from command line

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-clang-tidy-21 message/*.h message/*.c util/*.h util/*.c mcp2515/*.h mcp2515/*.c dspic33epxxxgp50x/*.h pic18f26k83/*.h --warnings-as-errors="*,-clang-diagnostic-c23-extensions" --checks="clang-*,misc-*"
+${CLANG_TIDY:-clang-tidy} message/*.h message/*.c util/*.h util/*.c mcp2515/*.h mcp2515/*.c dspic33epxxxgp50x/*.h pic18f26k83/*.h --warnings-as-errors="*,-clang-diagnostic-c23-extensions" --checks="clang-*,misc-*"
