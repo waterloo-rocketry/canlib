02dd087b5a4f517804851bd7769c0afb810f2cdb
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-10-02 23:17:01 -0400

    Add altitude data message test

--- tests/test_msg_sensor.cpp
+++ tests/test_msg_sensor.cpp
@@ -48,6 +48,48 @@ public:
 
 temp_data_msg_test temp_data_msg_test_inst;
 
+class altitude_data_msg_test : rocketry_test {
+public:
+	const char *get_name() override {
+		return "altitide_data_msg_test";
+	}
+
+	bool run_test() override {
+		bool test_passed = true;
+
+		can_msg_t msg;
+
+		can_msg_prio_t prio_before = test_rand<can_msg_prio_t, 0x3>();
+		std::uint16_t timestamp_before = test_rand<std::uint16_t>();
+		std::int32_t altitude_before = test_rand<std::int32_t>();
+		can_apogee_state_t apogee_state_before = test_rand<can_apogee_state_t, 0xff>();
+
+		build_altitude_data_msg(
+			prio_before, timestamp_before, altitude_before, apogee_state_before, &msg);
+
+		bool msg_is_sensor_data_after;
+		can_msg_type_t type_after;
+		std::uint16_t timestamp_after;
+		std::int32_t altitude_after;
+		can_apogee_state_t apogee_state_after;
+
+		msg_is_sensor_data_after = msg_is_sensor_data(&msg);
+		type_after = get_message_type(&msg);
+		timestamp_after = get_timestamp(&msg);
+		get_altitude_data(&msg, &altitude_after, &apogee_state_after);
+
+		test_assert(msg_is_sensor_data_after == true);
+		test_assert(type_after == MSG_SENSOR_ALTITUDE);
+		test_assert(timestamp_after == timestamp_before);
+		test_assert(altitude_after == altitude_before);
+		test_assert(apogee_state_after == apogee_state_before);
+
+		return test_passed;
+	}
+};
+
+altitude_data_msg_test altitude_data_msg_test_inst;
+
 class analog_sensor_message_test : rocketry_test {
 public:
 	const char *get_name() override {
