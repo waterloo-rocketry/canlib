d8613325dea8480aeb871d948deb92e8dcc8697d
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-10-04 10:57:34 -0400

    Add CRC8 function documentation

--- include/crc8.h
+++ include/crc8.h
@@ -8,7 +8,17 @@
 extern "C" {
 #endif
 
-uint8_t crc8_checksum(uint8_t *pdata, size_t nbytes, uint8_t crc);
+/**
+ * @brief Compute CRC8
+ *
+ * Computes the CRC8 checksum of input data
+ *
+ * @param pdata Input data buffer
+ * @param nbytes Buffer size in bytes
+ * @param crc CRC8 checksum of previous CRC8 calculation if calculating checksum on multiple discontinuous buffers, otherwise 0
+ * @return Computed CRC8 checksum
+ */
+uint8_t crc8_checksum(const uint8_t *pdata, size_t nbytes, uint8_t crc);
 
 #ifdef __cplusplus
 }
