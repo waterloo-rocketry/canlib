18144efc01fe7aab3768c8eee77e2286a9f0e18d
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-10-12 21:46:48 -0400

    Update various workflow

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-${CLANG_FORMAT:-clang-format} -i message/*.h message/*.c util/*.h util/*.c stm32h7/*.h stm32h7/*.c pic18f26k83/*.h pic18f26k83/*.c mcp2515/*.h mcp2515/*.c dspic33epxxxgp50x/*.h dspic33epxxxgp50x/*.c tests/*.cpp tests/*.hpp
+${CLANG_FORMAT:-clang-format} -i message/*.h message/*.c util/*.h util/*.c stm32h7/*.h stm32h7/*.c pic18f26k83/*.h pic18f26k83/*.c mcp2515/*.h mcp2515/*.c dspic33e/*.h dspic33e/*.c tests/*.cpp tests/*.hpp
