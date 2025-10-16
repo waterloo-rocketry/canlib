b0617c47c7600f3c002243e011e5c05dabdddefd
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-10-16 10:21:04 -0400

    Inline msg_common

--- message/msg_common.h
+++ message/msg_common.h
@@ -27,14 +27,42 @@
 extern "C" {
 #endif
 
-void write_timestamp_2bytes(uint16_t timestamp, can_msg_t *output);
+static inline void write_timestamp_2bytes(uint16_t timestamp, can_msg_t *output) {
+	output->data[0] = (timestamp >> 8) & 0xff;
+	output->data[1] = (timestamp >> 0) & 0xff;
+}
 
-can_msg_type_t get_message_type(const can_msg_t *msg);
+static inline can_msg_type_t get_message_type(const can_msg_t *msg) {
+	if (!msg) {
+		return (can_msg_type_t)0;
+	}
 
-uint8_t get_board_type_unique_id(const can_msg_t *msg);
-uint8_t get_board_inst_unique_id(const can_msg_t *msg);
+	return (can_msg_type_t)((msg->sid >> 18) & 0x1FF);
+}
 
-uint16_t get_timestamp(const can_msg_t *msg);
+static inline uint8_t get_board_type_unique_id(const can_msg_t *msg) {
+	if (!msg) {
+		return 0;
+	}
+
+	return (uint8_t)((msg->sid >> 8) & 0xFF);
+}
+
+static inline uint8_t get_board_inst_unique_id(const can_msg_t *msg) {
+	if (!msg) {
+		return 0;
+	}
+
+	return (uint8_t)(msg->sid & 0xFF);
+}
+
+static inline uint16_t get_timestamp(const can_msg_t *msg) {
+	if (!msg) {
+		return 0;
+	}
+
+	return ((uint16_t)msg->data[0] << 8) | msg->data[1];
+}
 
 #ifdef __cplusplus
 }
