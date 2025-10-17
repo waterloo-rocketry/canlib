9a21cc6a22ca9adf3af15fbec256c84a03a376b5
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-10-17 15:14:02 -0400

    Initial can_tx_buffer cleanup

--- util/can_tx_buffer.h
+++ util/can_tx_buffer.h
@@ -1,3 +1,6 @@
+#ifndef CANLIB_CAN_TX_BUFFER_H
+#define CANLIB_CAN_TX_BUFFER_H
+
 #include <stdbool.h>
 #include <stddef.h>
 
@@ -14,20 +17,25 @@ extern "C" {
  * can_tx_ready is a function that returns true if we can currently send a can
  * message, otherwise it returns false
  */
-void txb_init(void *pool, size_t pool_size, void (*can_send_fp)(const can_msg_t *),
+void txb_init(void *pool, size_t pool_size, void (*can_send)(const can_msg_t *),
 			  bool (*can_tx_ready)(void));
 
-/*
- * Buffers msg. If there is room in the can_tx buffer, this function will not
- * send the message. The only calls to can_send_fp will be made during
- * txb_heartbeat
+/**
+ * @brief Buffers message.
+ *
+ * If there is room in the can_tx buffer, this function will not send the message. The only calls to
+ * `can_send()` will be made during `txb_heartbeat()`
+ *
+ * @param msg message to be buffered
+ * @return `true` if success, `false` if failed(i.e. buffer is full)
  */
 bool txb_enqueue(const can_msg_t *msg);
 
-/*
- * This function is called every iteration through the main application loop.
- * If there are any messages that are in the "to transmit" queue, and
- * can_tx_ready() returns true, then can_send will be called with that message
+/**
+ * @brief This function is called every iteration through the main application loop.
+ *
+ * If there are any messages that are in the "to transmit" queue, and `can_tx_ready()` returns true,
+ * then `can_send()` will be called with that message
  */
 void txb_heartbeat(void);
 
@@ -35,3 +43,4 @@ void txb_heartbeat(void);
 }
 #endif
 
+#endif
