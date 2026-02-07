7400e90a3fbf9c44bffa463b3d44b9ddc4994c9a
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2026-02-07 18:57:53 -0500

    Update general, gps and recovery message format

--- message/msg_general.h
+++ message/msg_general.h
@@ -16,27 +16,27 @@ extern "C" {
  * corresponding supplemental data are defined in message_types.h.
  * This function may need to be modified to better hide the internals.
  */
-bool build_general_board_status_msg(can_msg_prio_t prio, uint16_t timestamp,
+void build_general_board_status_msg(can_msg_prio_t prio, uint16_t timestamp,
 									uint32_t general_error_bitfield,
 									uint16_t board_specific_error_bitfield, can_msg_t *output);
 
 /*
  * Used to Reset a CAN board
  */
-bool build_reset_msg(can_msg_prio_t prio, uint16_t timestamp, uint8_t board_type_id,
+void build_reset_msg(can_msg_prio_t prio, uint16_t timestamp, uint8_t board_type_id,
 					 uint8_t board_inst_id, can_msg_t *output);
 
 /*
  * Copies first 6 bytes of data
  */
-bool build_debug_raw_msg(can_msg_prio_t prio, uint16_t timestamp, const uint8_t *data,
+void build_debug_raw_msg(can_msg_prio_t prio, uint16_t timestamp, const uint8_t *data,
 						 can_msg_t *output);
 
-bool build_config_set_msg(can_msg_prio_t prio, uint16_t timestamp, uint8_t board_type_id,
+void build_config_set_msg(can_msg_prio_t prio, uint16_t timestamp, uint8_t board_type_id,
 						  uint8_t board_inst_id, uint16_t config_id, uint16_t config_value,
 						  can_msg_t *output);
 
-bool build_config_status_msg(can_msg_prio_t prio, uint16_t timestamp, uint16_t config_id,
+void build_config_status_msg(can_msg_prio_t prio, uint16_t timestamp, uint16_t config_id,
 							 uint16_t config_value, can_msg_t *output);
 
 bool get_general_board_status(const can_msg_t *msg, uint32_t *general_error_bitfield,
