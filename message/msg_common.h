3d3e0494f13100d000bc9f9628d54df8ef25e8a4
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2026-02-07 18:19:03 -0500

    Inline all msg_common function

--- message/msg_common.h
+++ message/msg_common.h
@@ -7,6 +7,8 @@
 #include "can.h"
 #include "message_types.h"
 
+#define assert(expr)
+
 #ifndef CANLIB_DYNAMIC_BOARD_ID
 
 #ifndef BOARD_TYPE_UNIQUE_ID
@@ -19,42 +21,86 @@
 #define BOARD_INST_UNIQUE_ID 0
 #endif
 
-#define SID(prio, msg)                                                                             \
-	(((uint32_t)(prio) << 27) | ((uint32_t)(msg) << 18) | ((uint32_t)BOARD_TYPE_UNIQUE_ID << 8) |  \
-	 BOARD_INST_UNIQUE_ID)
+static inline uint32_t build_sid(can_msg_prio_t prio, can_msg_type_t msg_type, uint8_t metadata) {
+	assert(((uint32_t)prio & 0xfffffffc) == 0);
+	assert(((uint32_t)msg_type & 0xffffff80) == 0);
+	assert(((uint32_t)BOARD_TYPE_UNIQUE_ID & 0xffffffc0) == 0);
+	assert(((uint32_t)BOARD_INST_UNIQUE_ID & 0xffffffc0) == 0);
+
+	return (((uint32_t)prio) << 27) | (((uint32_t)msg_type) << 20) |
+		   (((uint32_t)BOARD_TYPE_UNIQUE_ID) << 14) | (((uint32_t)BOARD_INST_UNIQUE_ID) << 8) |
+		   metadata;
+}
 
 #else
 
 extern uint8_t board_type_unique_id;
 extern uint8_t board_inst_unique_id;
 
-#define SID(prio, msg)                                                                             \
-	(((uint32_t)(prio) << 27) | ((uint32_t)(msg) << 18) | ((uint32_t)board_type_unique_id << 8) |  \
-	 board_inst_unique_id)
+static inline uint32_t build_sid(can_msg_prio_t prio, can_msg_type_t msg_type, uint8_t metadata) {
+	assert(((uint32_t)prio & 0xfffffffc) == 0);
+	assert(((uint32_t)msg_type & 0xffffff80) == 0);
+	assert(((uint32_t)board_type_unique_id & 0xffffffc0) == 0);
+	assert(((uint32_t)board_inst_unique_id & 0xffffffc0) == 0);
+
+	return (((uint32_t)prio) << 27) | (((uint32_t)msg_type) << 20) |
+		   (((uint32_t)board_type_unique_id) << 14) | (((uint32_t)board_inst_unique_id) << 8) |
+		   metadata;
+}
 
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+// SID Handling Functions
 
-void write_timestamp_2bytes(uint16_t timestamp, can_msg_t *output);
+static inline can_msg_type_t get_message_type(const can_msg_t *msg) {
+	assert(msg);
 
-can_msg_type_t get_message_type(const can_msg_t *msg);
+	return (can_msg_type_t)((msg->sid >> 20) & 0x7f);
+}
 
-uint8_t get_board_type_unique_id(const can_msg_t *msg);
-uint8_t get_board_inst_unique_id(const can_msg_t *msg);
+static inline uint8_t get_board_type_unique_id(const can_msg_t *msg) {
+	assert(msg);
 
-uint16_t get_timestamp(const can_msg_t *msg);
+	return (msg->sid >> 14) & 0x3f;
+}
 
+static inline uint8_t get_board_inst_unique_id(const can_msg_t *msg) {
+	assert(msg);
+
+	return (msg->sid >> 8) & 0x3f;
+}
+
+static inline uint8_t get_message_metadata(const can_msg_t *msg) {
+	assert(msg);
+
+	return msg->sid & 0xff;
+}
+
+// Timestamp handle functions
+static inline void write_timestamp(uint16_t timestamp, can_msg_t *output) {
+	assert(output);
+
+	output->data[0] = (timestamp >> 8) & 0xff;
+	output->data[1] = (timestamp >> 0) & 0xff;
+}
+
+static inline uint16_t get_timestamp(const can_msg_t *msg) {
+	assert(msg);
+
+	return ((uint16_t)msg->data[0] << 8) | msg->data[1];
+}
+
+// Dynamic board ID specific function
 #ifdef CANLIB_DYNAMIC_BOARD_ID
 
-void set_board_id(uint8_t board_type_id, uint8_t board_inst_id);
+static inline void set_board_id(uint8_t board_type_id, uint8_t board_inst_id) {
+	assert(((uint32_t)board_type_id & 0xc0) == 0);
+	assert(((uint32_t)board_inst_id & 0xc0) == 0);
 
-#endif
-
-#ifdef __cplusplus
+	board_type_unique_id = board_type_id;
+	board_inst_unique_id = board_inst_id;
 }
+
 #endif
 
 #endif
