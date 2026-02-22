2249cc4b82358f49ce9c211de3297ae74359a8f3
E. L. Raby <eleoraby@gmail.com>
2026-02-22 01:23:50 -0500

    feat: gps timestamp test

---
+++ tests/test_msg_gps.cpp
@@ -0,0 +1,70 @@
+#include <cstdint>
+
+#include "rockettest.hpp"
+
+#include "can.h"
+#include "message/msg_common.h"
+#include "message/msg_gps.h"
+#include "message_types.h"
+
+class gps_timestamp_message_test : rockettest_test {
+public:
+    gps_timestamp_message_test() : rockettest_test("gps_timestamp_message_test") {}
+
+    bool run_test() override {
+        bool test_passed = true;
+
+        can_msg_t msg;
+
+        can_msg_prio_t prio_before = rockettest_rand<can_msg_prio_t, 0x3>(); 
+        std::uint16_t timestamp_before = rockettest_rand<std::uint16_t>(); //byte 0-1
+        std::uint8_t utc_hours_before = rockettest_rand<std::uint8_t, 0xff>(); //byte 2
+        std::uint8_t utc_mins_before = rockettest_rand<std::uint8_t, 0xff>(); //byte 3
+        std::uint8_t utc_secs_before = rockettest_rand<std::uint8_t, 0xff>(); //byte 4
+        std::uint8_t utc_dsecs_before = rockettest_rand<std::uint8_t, 0xff>(); //byte 5
+
+        build_gps_time_msg(prio_before, timestamp_before, utc_hours_before, utc_mins_before, utc_secs_before, utc_dsecs_before, &msg);
+
+        std::uint16_t timestamp_extracted;
+        std::uint8_t utc_hours_extracted;
+        std::uint8_t utc_mins_extracted;
+        std::uint8_t utc_secs_extracted;
+        std::uint8_t utc_dsecs_extracted;
+
+        timestamp_extracted = (static_cast<std::uint16_t>(msg.data[0]) << 8) | msg.data[1];
+        utc_hours_extracted = msg.data[2];
+        utc_mins_extracted = msg.data[3];
+        utc_secs_extracted = msg.data[4];
+        utc_dsecs_extracted = msg.data[5];
+
+        rockettest_check_expr_true(msg.data_len == 6);
+        rockettest_check_expr_true(timestamp_extracted == timestamp_before);
+        rockettest_check_expr_true(utc_hours_extracted == utc_hours_before);
+        rockettest_check_expr_true(utc_mins_extracted == utc_mins_before);
+        rockettest_check_expr_true(utc_secs_extracted == utc_secs_before);
+        rockettest_check_expr_true(utc_dsecs_extracted == utc_dsecs_before);
+        rockettest_check_expr_true(timestamp_extracted == timestamp_before);
+
+        can_msg_type_t type_after;
+        std::uint16_t timestamp_after;
+        uint8_t utc_hours_after;
+        uint8_t utc_mins_after;
+        uint8_t utc_secs_after;
+        uint8_t utc_dsecs_after;
+
+        type_after = get_message_type(&msg);
+        timestamp_after = get_timestamp(&msg);
+        get_gps_time(&msg, &utc_hours_after, &utc_mins_after, &utc_secs_after, &utc_dsecs_after);
+
+        rockettest_check_expr_true(type_after == MSG_GPS_TIMESTAMP);
+        rockettest_check_expr_true(timestamp_after == timestamp_before);
+        rockettest_check_expr_true(utc_hours_after == utc_hours_before);
+        rockettest_check_expr_true(utc_mins_after == utc_mins_before);
+        rockettest_check_expr_true(utc_secs_after == utc_secs_before);
+        rockettest_check_expr_true(utc_dsecs_after == utc_dsecs_before);
+
+        return test_passed;
+    }
+};
+
+gps_timestamp_message_test gps_timestamp_message_test_inst;
